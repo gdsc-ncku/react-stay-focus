dc3401ccf4c0a0ce8fb151d7d4d8f9e895ee0fcb
heyihuang826 <heyihuang826@gmail.com>
2024-04-11 19:29:33 +0800

    add CardWithLogo

--- src/sharedComponents/CardWithLogo.js
+++ src/sharedComponents/CardWithLogo.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import { Card, CardHeader, CardContent } from '@material-ui/core'; // Assuming you're using Material-UI for the card components
 
-const CardWithLogo = ({ children }) => {
+const SharedCard = ({ header, media, actions, children }) => {
   return (
-    <Card style={{ maxWidth: '40%', margin: 'auto', position: 'relative', top: '40px', textAlign: 'center' }}>
-      <CardHeader className="card-header">
-        <div id="logo-div"><img alt="logo" src="../images/logo-red.png" /></div>
-        {children.header}
-      </CardHeader>
-      {children.media && <div>{children.media}</div>}
-      <CardContent>
-        {children.content}
-      </CardContent>
+    <Card style={{borderRadius: '4px'}}>
+      {header && 
+        
+        <CardHeader className={card-header} title={header}>
+          <div id="logo-div"><img alt="logo" src="../images/logo-red.png" /></div>
+        </CardHeader>
+        }
+      
+      {media && 
+        {media}
+        }
+        
+      
+      <CardContent>{children}</CardContent>
     </Card>
   );
 };
