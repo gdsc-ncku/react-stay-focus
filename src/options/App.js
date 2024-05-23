bc74754004fb7332ae3c5c91a178890294ee4e46
peterxcli <peterxcli@gmail.com>
2024-05-23 23:30:10 +0800

    feat: Add reset button functionality

--- src/options/App.js
+++ src/options/App.js
@@ -30,82 +30,82 @@ import UnlockPage from "./unlock/UnlockPage";
 // import SocialMediaShare from "../sharedComponents/SocialMediaShare";
 
 export default class App extends Component {
-    drawerWidth = 240;
+  drawerWidth = 240;
 
-    constructor(props) {
-      super(props)
-      this.state = {
-        selectedTab: "block-by-website-tab",
-        active: false,
-        lockType: "none",
-        isLocked: false,
-        loading: true
-      }
+  constructor(props) {
+    super(props)
+    this.state = {
+      selectedTab: "block-by-website-tab",
+      active: false,
+      lockType: "none",
+      isLocked: false,
+      loading: true
     }
-    componentDidMount() {
-      this.loadData()
-      this.state.loading = false
-      console.log("loading success")
-    }
-    selectTab(tabName) {
-      console.log("now setting to", tabName);
-      this.setState({selectedTab : tabName});
-    }
-    isSelectedTab(tabName) {
-      return this.state.selectedTab === tabName
-    }
-    handleUnlock = () => {
-      this.setState({isLocked : false})
-      this.setState({active : false})
-      console.log("unlock was invoked success  in app.js", this.state.isLocked);
-    }
-    loadData = () => {
-      localStorage.get("active")
-        .then(active => {
-          this.setState({ active: active })
-          console.log("active", active)
-        })
-        .catch((error) => {
-          console.error("獲取資料失敗ha ha:", error);
-          // 在這裡可以處理錯誤情況
-        });
-      localStorage.get("settings").then(settings => {
-        this.setState({ lockType: settings.lock.type })
-        this.setState({
-          isLocked:
-            this.state.lockType !== "none" &&
-            (this.state.active === true || this.state.lockType === "password")
-        })
+  }
+  componentDidMount() {
+    this.loadData()
+    this.state.loading = false
+    console.log("loading success")
+  }
+  selectTab(tabName) {
+    console.log("now setting to", tabName);
+    this.setState({ selectedTab: tabName });
+  }
+  isSelectedTab(tabName) {
+    return this.state.selectedTab === tabName
+  }
+  handleUnlock = () => {
+    this.setState({ isLocked: false })
+    this.setState({ active: false })
+    console.log("unlock was invoked success  in app.js", this.state.isLocked);
+  }
+  loadData = () => {
+    localStorage.get("active")
+      .then(active => {
+        this.setState({ active: active })
+        console.log("active", active)
       })
-    }
+      .catch((error) => {
+        console.error("獲取資料失敗ha ha:", error);
+        // 在這裡可以處理錯誤情況
+      });
+    localStorage.get("settings").then(settings => {
+      this.setState({ lockType: settings.lock.type })
+      this.setState({
+        isLocked:
+          this.state.lockType !== "none" &&
+          (this.state.active === true || this.state.lockType === "password")
+      })
+    })
+  }
 
-    saveActive(active) {
-      localStorage.set("active", active);
-      localStorage.get("active")
-          .then(active => {
-          console.log("active is getting", active);
-          })
-      // setIcon(active);
-    };
+  saveActive(active) {
+    localStorage.set("active", active);
+    localStorage.get("active")
+      .then(active => {
+        console.log("active is getting", active);
+      })
+    // setIcon(active);
+  };
 
-    handlchange = (active) => {
-      this.setState({active: !active});
-    }
+  handlchange = (active) => {
+    this.setState({ active: !active });
+  }
 
-    generateTab = (text, text_id, icon) => {
-      return <List>
-        <ListItem>
-          <ListItemButton onClick={() => this.selectTab(text_id)}>
-            <ListItemIcon>
-              {icon}
-            </ListItemIcon>
-            <ListItemText primary={text} />
-          </ListItemButton>
-        </ListItem>
-      </List>;
-    }
+  generateTab = (text, text_id, icon) => {
+    return <List>
+      <ListItem>
+        <ListItemButton onClick={() => this.selectTab(text_id)}>
+          <ListItemIcon>
+            {icon}
+          </ListItemIcon>
+          <ListItemText primary={text} />
+        </ListItemButton>
+      </ListItem>
+    </List>;
+  }
 
-    render() {
+  render() {
     return (
       <div className='page-container'>
         {this.state.loading && (
@@ -119,27 +119,29 @@ export default class App extends Component {
         {!this.state.loading && !this.state.isLocked && (
           <Box sx={{ display: 'flex' }}>
             <CssBaseline />
-            <AppBar position="fixed" sx={{width: { sm: `calc(100% - ${this.drawerWidth}px)` , }, 
-                                                      ml: { sm: `${this.drawerWidth}px` }}}>
+            <AppBar position="fixed" sx={{
+              width: { sm: `calc(100% - ${this.drawerWidth}px)`, },
+              ml: { sm: `${this.drawerWidth}px` }
+            }}>
               <Toolbar>
                 <Typography className='md-title' sx={{ flexGrow: 1 }} noWrap component="div">Stay Focused</Typography>
-                <FormControlLabel control={<Switch checked={this.state.active} onChange={(e) => {this.saveActive(e.target.checked), this.handlchange(this.state.active)}}
-                                                    color="warning"/>}
-                                  label={this.state.active? "Active": "Inactive"}/>
+                <FormControlLabel control={<Switch checked={this.state.active} onChange={(e) => { this.saveActive(e.target.checked), this.handlchange(this.state.active) }}
+                  color="warning" />}
+                  label={this.state.active ? "Active" : "Inactive"} />
               </Toolbar>
             </AppBar>
-            
+
             <Box
               component="nav"
               sx={{ width: { sm: this.drawerWidth }, flexShrink: { sm: 0 } }}
               aria-label="mailbox folders"
             >
               <Drawer anchor="left"
-                      variant="permanent"
-                      sx={{
-                        display: 'block',
-                        '& .MuiDrawer-paper': { boxSizing: 'border-box', width: this.drawerWidth },
-                      }}>
+                variant="permanent"
+                sx={{
+                  display: 'block',
+                  '& .MuiDrawer-paper': { boxSizing: 'border-box', width: this.drawerWidth },
+                }}>
 
                 {this.generateTab("Block by Website", "block-by-website-tab", <MoveToInboxIcon />)}
                 {this.generateTab("Block By Word", "block-by-word-tab", <StickyNote2Icon />)}
@@ -147,38 +149,45 @@ export default class App extends Component {
                 {this.generateTab("Settings", "settings-tab", <SettingsIcon />)}
                 {this.generateTab("About", "about-tab", <InfoIcon />)}
               </Drawer>
-              </Box>
+            </Box>
 
-              <Box component="nav" sx={{ display: 'flex', paddingTop: 5}}>
-                  {this.state.selectedTab == "block-by-website-tab" && (
-                    <div>
-                      <BlockByWebsiteTab/>
-                    </div>
-                  )}
+            <Box component="nav" sx={{ display: 'flex', paddingTop: 5 }}>
+              {this.state.selectedTab == "block-by-website-tab" && (
+                <div>
+                  <BlockByWebsiteTab />
+                </div>
+              )}
 
-                  {this.state.selectedTab == "block-by-word-tab" && (
-                    <div>
-                      <BlockByWordTab/>
-                    </div>
-                  )}
+              {this.state.selectedTab == "block-by-word-tab" && (
+                <div>
+                  <BlockByWordTab />
+                </div>
+              )}
 
-                  {this.state.selectedTab == "block-by-regex-tab" && (
-                    <div>
-                      <BlockByRegexTab/>
-                    </div>
-                  )}
+              {this.state.selectedTab == "block-by-regex-tab" && (
+                <div>
+                  <BlockByRegexTab />
+                </div>
+              )}
 
-                  {this.state.selectedTab == "settings-tab" && (
-                    <div>
-                      <SettingsTab reloadData={this.loadData}/>
-                    </div>
-                  )}
-              </Box>
+              {this.state.selectedTab == "settings-tab" && (
+                <div>
+                  <SettingsTab reloadData={this.loadData} />
+                </div>
+              )}
+
+              {
+                this.state.selectedTab == "about-tab" && (
+                  <div>
+                    <AboutTab />
+                  </div>
+                )}
+            </Box>
           </Box>
         )}
       </div>
     )
-    }
+  }
 }
 
 // const ComponentReloadData = ({ reloadData, selectedTab }) => {
